e055b96e9b9046bede98918d7bc84ea32a3102ab
Nina Zyabrina <zyabrina95@gmail.com>
2024-12-22 15:41:11 +0100

    send phones and domains req

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,10 +1,19 @@
 import fastify from 'fastify';
 
+const data = {
+  phones: ['+12345678', '3434343434', '234-56-78'],
+  domains: ['example.com', 'hexlet.io'],
+};
+
 const app = fastify();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/phones', (req, res) => {
+  res.send(data.phones);
+});
+
+app.get('/domains', (req, res) => {
+  res.send(data.domains);
 });
 
 app.listen({ port }, () => {
